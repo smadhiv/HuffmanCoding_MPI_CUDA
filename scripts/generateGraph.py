788a6089a66d56f2875516c867bf6422efb70e13
Sriram Madhivanan <sriram.inuyasha@gmail.com>
2016-08-31 14:29:06 -0400

    Update generateGraph.py

--- scripts/generateGraph.py
+++ scripts/generateGraph.py
@@ -2,11 +2,9 @@ resourceUsed = []
 architectureName = []
 runTime = dict()
 avgTime = dict()
+finalAvgTime = dict()
 
-import os
-os.chdir('./test/')
-fileList = os.listdir('.')
-for file in fileList:
+def getHeaderInformation(name):
   print ("processing file " + file)
   with open(file, 'r') as fileText:
     text = fileText.readlines()
@@ -14,23 +12,37 @@ for file in fileList:
     architectureName.extend(text[0].split()[-1:])
     resourceUsed.extend(text[1].split()[-1:])
     compressionFileSize = str(text[2].split()[-1:])
-    tempTime = []
-    for index in list(range(3, len(text))):
-      tempList = text[index].split()
-      if tempList[0] == 'FileSize:':
-        runTime[compressionFileSize] = tempTime
-        avgTime[compressionFileSize] = round(sum(tempTime)/len(tempTime), 3)
-        compressionFileSize = str(tempList[1])
-        tempTime = []
-      elif tempList[0] == 'Time':
-        tempTime.append(float(tempList[2][:-4] + '.' + tempList[2][-3:]))
-      else:
-        break
-    runTime[compressionFileSize] = tempTime
-    avgTime[compressionFileSize] = round(sum(tempTime)/len(tempTime), 3)
-    compressionFileSize = str(tempList[1])
-    print (architectureName)
-    print (avgTime)
+    return text, compressionFileSize
+
+def calculateRunTimeAvg(text, compressionFileSize):
+  tempTime = []
+  for index in list(range(3, len(text))):
+    tempList = text[index].split()
+    if tempList[0] == 'FileSize:':
+      runTime[compressionFileSize] = tempTime
+      avgTime[compressionFileSize] = round(sum(tempTime)/len(tempTime), 3)
+      compressionFileSize = str(tempList[1])
+      tempTime = []
+    elif tempList[0] == 'Time':
+      tempTime.append(float(str(tempList[2]).replace(":", ".")))
+    else:
+      break
+  runTime[compressionFileSize] = tempTime
+  avgTime[compressionFileSize] = round(sum(tempTime)/len(tempTime), 3)
+  compressionFileSize = str(tempList[1])
+  finalAvgTime[str(text[0].split()[-1:])] = avgTime
+
+def processFile(name):
+  text, compressionFileSize = getHeaderInformation(name)
+  calculateRunTimeAvg(text, compressionFileSize)
+  
+import os
+os.chdir('./test/')
+fileList = os.listdir('.')
+for file in fileList:
+  processFile(file)
+print (architectureName)
+print (finalAvgTime)
 
     
   
